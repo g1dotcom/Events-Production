c20f283a7ac7bf1b1bc76764a79ebdc4c0bb182f
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-22 15:36:23 +0300

    feat: Add Social Platforms and CitiesSelectModal components to Countries page

--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -1,9 +1,11 @@
+import SoacialPlatform from "@/components/social-platforms";
 import CitiesSelectModal from "./_components/cities-select-modals";
 
 const Countries = () => {
   return (
     <div>
       <CitiesSelectModal />
+      <SoacialPlatform />
     </div>
   );
 };
