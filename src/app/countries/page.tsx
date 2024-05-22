362a6b0e3a6f1857ed5e73c85b100825f7cc9a7d
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-22 19:02:18 +0300

    feat: Add Events component to Countries page

--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -1,11 +1,13 @@
 import SoacialPlatform from "@/components/social-platforms";
 import CitiesSelectModal from "./_components/cities-select-modals";
+import Events from "@/components/events";
 
 const Countries = () => {
   return (
     <div>
       <CitiesSelectModal />
       <SoacialPlatform />
+      <Events />
     </div>
   );
 };
