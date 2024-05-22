ce010e46ac75c871479e72293ab7dd0336de89de
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-22 20:33:35 +0300

    chore: Update Map component with improved imports and styling

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,28 +1,39 @@
 "use client";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 
-type Props = {};
+// START: Preserve spaces to avoid auto-sorting
+import "leaflet/dist/leaflet.css";
 
-const Map = (props: Props) => {
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
+
+import "leaflet-defaulticon-compatibility";
+// END: Preserve spaces to avoid auto-sorting
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+export default function Map() {
   return (
-    <div className="w-full flex justify-center ">
-      <MapContainer
-        className="w-[800px] h-[500px]"
-        center={[51.505, -0.09]}
-        zoom={13}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
+    <MapContainer
+      preferCanvas={true}
+      center={[51.505, -0.09]}
+      zoom={11}
+      scrollWheelZoom={true}
+      style={{ height: "400px", width: "%100" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          This Marker icon is displayed correctly with{" "}
+          <i>leaflet-defaulticon-compatibility</i>.
+        </Popup>
+      </Marker>
+      <Marker position={[38.508156, 27.07854]}>
+        <Popup>
+          This Marker icon is displayed correctly with{" "}
+          <i>leaflet-defaulticon-compatibility</i>.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
-};
-export default Map;
+}
