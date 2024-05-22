3c8a5504dc683b89e1a6c636bf9b07db1e5495e3
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-22 21:27:43 +0300

    feat: Fetch and display event locations on the map

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -8,27 +8,49 @@ import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import "leaflet-defaulticon-compatibility";
 // END: Preserve spaces to avoid auto-sorting
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useEffect, useState } from "react";
+import supabase from "@/lib/subaseClient";
 
 export default function Map() {
+  const [latitude, setLatitude] = useState<number>(0);
+  const [longitude, setLongitude] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchLocations = async () => {
+      const { data, error } = await supabase.from("events").select("*");
+      if (data) {
+        // console.log(data[0].latitude, data[0].longitude);
+        setLatitude(data[0].latitude);
+        setLongitude(data[0].longitude);
+      } else {
+        console.error(error);
+      }
+    };
+
+    fetchLocations();
+  }, []);
+
+  const position: [number, number] = [longitude, latitude];
+
   return (
     <MapContainer
       preferCanvas={true}
       center={[51.505, -0.09]}
       zoom={11}
       scrollWheelZoom={true}
-      style={{ height: "400px", width: "%100" }}
+      style={{ height: "400px", width: "100%" }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      {/* <Marker position={[51.505, -0.09]}>
         <Popup>
           This Marker icon is displayed correctly with{" "}
           <i>leaflet-defaulticon-compatibility</i>.
         </Popup>
-      </Marker>
-      <Marker position={[38.508156, 27.07854]}>
+      </Marker> */}
+      <Marker position={position}>
         <Popup>
           This Marker icon is displayed correctly with{" "}
           <i>leaflet-defaulticon-compatibility</i>.
