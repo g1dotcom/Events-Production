fb6b6d88a59c86b95c1e61a1ea1dbf2554940c06
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-22 04:13:23 +0300

    feat: Add CitiesSelectModal component to Countries page

---
+++ src/app/countries/page.tsx
@@ -0,0 +1,11 @@
+import CitiesSelectModal from "./_components/cities-select-modals";
+
+const Countries = () => {
+  return (
+    <div>
+      <CitiesSelectModal />
+    </div>
+  );
+};
+
+export default Countries;
