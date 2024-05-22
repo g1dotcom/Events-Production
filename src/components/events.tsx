98d463008ecf25f5379236d1a487f3b6b41f8a76
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-22 19:02:06 +0300

    feat: Add Events component for displaying events

---
+++ src/components/events.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import supabase from "@/lib/subaseClient";
+
+type Props = {};
+
+const Events = (props: Props) => {
+  const [events, setEvents] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const { data, error } = await supabase.from("events").select("*");
+      if (data) {
+        setEvents(data);
+        console.log(events);
+      } else {
+        console.error(error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  return (
+    <div>
+      <h1>Events</h1>
+      <ul>
+        {events.map((event) => (
+          <li key={event.id}>
+            {event.name} {event.city_id} {event.base_portal_link}{" "}
+            {event.social_platform_id} {event.event_type_id} {event.language}{" "}
+            {event.local_time}
+            {event.time_zone} {event.utc_time} {event.longitute}{" "}
+            {event.latitude}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Events;
