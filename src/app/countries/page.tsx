1397987bf7268ad31e1721fee453a8b9e85d96b2
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-24 02:19:59 +0300

    feat: Add EventsTable component for displaying events in a table format

--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -2,14 +2,16 @@ import SoacialPlatform from "@/components/social-platforms";
 import CitiesSelectModal from "./_components/cities-select-modals";
 import Events from "@/components/events";
 import Map from "@/components/map";
+import { EventsTable } from "@/components/events-table";
 
 const Countries = () => {
   return (
     <div>
       {/* <CitiesSelectModal /> */}
       {/* <SoacialPlatform /> */}
-      <Events />
+      {/* <Events /> */}
       <Map />
+      <EventsTable />
     </div>
   );
 };
