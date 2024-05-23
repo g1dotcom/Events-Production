a8618c935e94029aeec3c00a5b6860c07a3f6626
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-24 02:20:02 +0300

    feat: Update EventsTable component to fetch and display events from the database

--- src/components/events-table.tsx
+++ src/components/events-table.tsx
@@ -1,9 +1,109 @@
-import React from "react";
+"use client";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import supabase from "@/lib/subaseClient";
+import { useState, useEffect } from "react";
 
-type Props = {};
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+];
 
-const EventsTable = (props: Props) => {
-  return <div>EventsTable</div>;
-};
+export function EventsTable() {
+  const [events, setEvents] = useState<any[]>([]);
 
-export default EventsTable;
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const { data, error } = await supabase.from("events").select("*");
+      if (data) {
+        setEvents(data);
+        console.log(data);
+      } else {
+        console.error(error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+  return (
+    <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>City</TableHead>
+          <TableHead>Base Portal</TableHead>
+          <TableHead>Socail Platform</TableHead>
+          <TableHead>Event Type</TableHead>
+          <TableHead>Language</TableHead>
+          <TableHead>Local Time</TableHead>
+          <TableHead>Time Zone</TableHead>
+          <TableHead>UTC Time</TableHead>
+          <TableHead>Countdown</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {events.map((event) => (
+          <TableRow key={event.id}>
+            <TableCell className="font-medium">{event.city_id}</TableCell>
+            {/* <TableCell>{invoice.paymentStatus}</TableCell>
+            <TableCell>{invoice.paymentMethod}</TableCell> */}
+          </TableRow>
+        ))}
+      </TableBody>
+      {/* <TableFooter>
+        <TableRow>
+          <TableCell colSpan={3}>Total</TableCell>
+          <TableCell className="text-right">$2,500.00</TableCell>
+        </TableRow>
+      </TableFooter> */}
+    </Table>
+  );
+}
