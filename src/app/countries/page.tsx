2bbdd8db3808ec9988e4fa908a42073f946d42a0
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-24 02:19:54 +0300

    chore: Remove EventsTable component and commented out code for social platform and events components

--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -2,16 +2,14 @@ import SoacialPlatform from "@/components/social-platforms";
 import CitiesSelectModal from "./_components/cities-select-modals";
 import Events from "@/components/events";
 import Map from "@/components/map";
-import EventsTable from "@/components/events-table";
 
 const Countries = () => {
   return (
     <div>
-      <CitiesSelectModal />
-      {/* <SoacialPlatform />
-      <Events /> */}
+      {/* <CitiesSelectModal /> */}
+      {/* <SoacialPlatform /> */}
+      <Events />
       <Map />
-      <EventsTable />
     </div>
   );
 };
