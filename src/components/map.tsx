aa8325ee69092e873c3d1c399dddcd8ea79eb1fe
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-24 14:22:33 +0300

    refactor: Update map layout and optimize code for Map component

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -36,43 +36,37 @@ export default function Map() {
   const position: [number, number] = [latitude, longitude];
 
   return (
-    <div className="w-full flex justify-center items-center">
-      <MapContainer
-        preferCanvas={true}
-        center={[35, 0]}
-        zoom={1}
-        scrollWheelZoom={true}
-        style={{
-          height: "400px",
-          width: "50%",
-          borderTopRightRadius: "100px",
-          borderBottomLeftRadius: "100px",
-          borderBottomRightRadius: "100px",
-        }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {/* <Marker position={[51.505, -0.09]}>
+    <MapContainer
+      preferCanvas={true}
+      center={[35, 0]}
+      zoom={1}
+      scrollWheelZoom={true}
+      style={{
+        height: "400px",
+        width: "100%",
+      }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {/* <Marker position={[51.505, -0.09]}>
         <Popup>
           This Marker icon is displayed correctly with{" "}
           <i>leaflet-defaulticon-compatibility</i>.
         </Popup>
       </Marker> */}
-        {eventsData.map((event) => (
-          <Marker key={event.id} position={[event.longitude, event.latitude]}>
-            <Popup>
-              {event.name} {event.city_id} {event.base_portal_link}{" "}
-              {event.social_platform_id} {event.event_type_id} {event.language}{" "}
-              {event.local_time}
-              {event.time_zone} {event.utc_time} {event.longitute}{" "}
-              {event.latitude}
-            </Popup>
-          </Marker>
-        ))}
-      </MapContainer>
-      <FlipWordsTitle />
-    </div>
+      {eventsData.map((event) => (
+        <Marker key={event.id} position={[event.longitude, event.latitude]}>
+          <Popup>
+            {event.name} {event.city_id} {event.base_portal_link}{" "}
+            {event.social_platform_id} {event.event_type_id} {event.language}{" "}
+            {event.local_time}
+            {event.time_zone} {event.utc_time} {event.longitute}{" "}
+            {event.latitude}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
   );
 }
