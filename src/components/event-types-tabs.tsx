51c146d347280ede63b990e67b8aa90408d7e8f0
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-25 01:17:11 +0300

    feat: Update Anomaly event type in EventTypesTabs component

--- src/components/event-types-tabs.tsx
+++ src/components/event-types-tabs.tsx
@@ -1,117 +1,106 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 import Image from "next/image";
 
 //images
-import Anomally from "../../public/events-types/xmanomaly.png";
-import AnomallyBanner from "../../public/events-types/xm-anomaly-photo.jpg";
+import AnomallyBanner from "../../public/events/xm-anomaly-photo.jpg";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export function EventTypesTabs() {
-  const [activeTab, setActiveTab] = useState<any>([]);
+  const [activeTab, setActiveTab] = useState<string>("");
 
   const eventTypes = [
     {
       name: "Anomaly",
-      value: "anomaly",
+      value: "xmanomaly",
+      description:
+        "An Anomaly is a series of events in which two factions compete for control of the local XM network. The winning faction will gain special rewards.",
     },
     {
       name: "Mission Day",
       value: "missionday",
+      description:
+        "Mission Day is an event where players explore the world around them by completing missions.",
     },
     {
       name: "First Saturday",
       value: "firstsaturday",
+      description:
+        "First Saturday is a casual meetup for Ingress players to socialize, hack portals, and work on their mission badges.",
+    },
+
+    {
+      name: "NL-1331",
+      value: "nl1331",
+      description:
+        "First Saturday is a casual meetup for Ingress players to socialize, hack portals, and work on their mission badges.",
+    },
+    {
+      name: "Special Events",
+      value: "specialevents",
+      description:
+        "First Saturday is a casual meetup for Ingress players to socialize, hack portals, and work on their mission badges.",
+    },
+    {
+      name: "TKO",
+      value: "tko",
+      description:
+        "First Saturday is a casual meetup for Ingress players to socialize, hack portals, and work on their mission badges.",
+    },
+    {
+      name: "Hexathlon",
+      value: "hexathlon1",
+      description:
+        "First Saturday is a casual meetup for Ingress players to socialize, hack portals, and work on their mission badges.",
     },
   ];
 
-  console.log(activeTab);
-
-  //   const SpanHover = "border-b-2 border-white";
-  // className={`w-full lg:w-1/6 md:rounded-l-3xl ${   darkMode ? "dark-light-info shadow-xl shadow-black" : "bg-main-left"  }`}
+  const getActiveEvent = () =>
+    eventTypes.find((event) => event.value === activeTab);
 
   return (
-    <div className="w-full  flex  justify-center">
-      <div className="w-full  flex flex-col items-center space-y-10  h-[450px] relative">
-        <button
-          onClick={() => setActiveTab(eventTypes[0].value)}
-          value={eventTypes[0].value}
-          className="text-white flex flex-col items-center w-40  hover:border-white border-[#783cbd] border-b-2 px-4 cursor-pointer"
-        >
-          <Image width={40} src={Anomally} alt="" />
-          <h1>ANOMALY</h1>
-        </button>
-        <div className="w-full flex justify-center text-white absolute">
-          {activeTab === eventTypes[0].value && (
-            <div className="w-2/3 flex justify-center space-x-10">
-              <Image width={350} src={AnomallyBanner} alt="" />
-              <p>
-                An Anomaly is a series of events in which two factions compete
-                for control of the local XM network. The winning faction will
-                gain special rewards.
-              </p>
+    <div className="w-full relative flex flex-col items-center">
+      <div className="w-full flex justify-center   p-5">
+        {eventTypes.map((event) => (
+          <button
+            key={event.name}
+            onClick={() => setActiveTab(event.value)}
+            className={`text-white flex flex-col items-center w-40 hover:border-white border-b-2  cursor-pointer ${
+              activeTab === event.value ? "border-white" : "border-[#783cbd]"
+            }`}
+          >
+            <div
+              style={{
+                width: 100,
+                height: 100,
+                position: "relative",
+                marginBottom: 10,
+              }}
+            >
+              <Image
+                src={`/events/${event.value}.png`}
+                layout="fill"
+                objectFit="contain"
+                alt={event.name}
+              />
             </div>
-          )}
-        </div>
+            <h1>{event.name}</h1>
+          </button>
+        ))}
       </div>
-      <div className="w-full  flex flex-col items-center space-y-10  h-[450px] relative">
-        <button
-          onClick={() => setActiveTab(eventTypes[1].value)}
-          value={eventTypes[1].value}
-          className="text-white flex flex-col items-center w-40  hover:border-white border-[#783cbd] border-b-2 px-4 cursor-pointer"
-        >
-          <Image width={40} src={Anomally} alt="" />
-          <h1>ANOMALY</h1>
-        </button>
-        <div className="w-full flex justify-center text-white">
-          {activeTab === eventTypes[1].value && (
-            <div className="w-2/3 flex justify-center space-x-10">
-              <Image width={350} src={AnomallyBanner} alt="" />
-              <p>
-                An Anomaly is a series of events in which two factions compete
-                for control of the local XM network. The winning faction will
-                gain special rewards.
-              </p>
-            </div>
-          )}
-        </div>
+      <div className="w-full flex justify-center text-white  mt-5 p-5">
+        {getActiveEvent() && (
+          <div className="w-full flex justify-center space-x-10">
+            <Image
+              width={350}
+              src={AnomallyBanner}
+              alt={getActiveEvent()?.name ?? ""}
+            />
+            <p>{getActiveEvent()?.description}</p>
+          </div>
+        )}
       </div>
-
-      {/* <button
-        onClick={() => setActiveTab(eventTypes[1].value)}
-        value={eventTypes[1].value}
-        className="text-white flex flex-col items-center  hover:border-white border-[#783cbd] border-b-2 px-4 cursor-pointer"
-      >
-        <Image width={40} src={Anomally} alt="" />
-        <h1>Mission Day</h1>
-        {activeTab === eventTypes[1].value && (
-          <div className="w-1/6 h-1 bg-white">Mission Day</div>
-        )}
-      </button>
-
-      <button
-        onClick={() => setActiveTab(eventTypes[2].value)}
-        value={eventTypes[2].value}
-        className="text-white flex flex-col items-center  hover:border-white border-[#783cbd] border-b-2 px-4 cursor-pointer"
-      >
-        <Image width={40} src={Anomally} alt="" />
-        <h1>First Saturday</h1>
-        {activeTab === eventTypes[2].value && (
-          <div className="w-1/6 h-1 bg-white">First Saturday</div>
-        )}
-      </button> */}
     </div>
   );
 }
