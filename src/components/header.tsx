a7ed8b246dbbdaa17bfa42780ea2afc435eec246
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-24 18:03:31 +0300

    refactor: Update header component with responsive design and improved text alignment

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,16 +4,15 @@ import { EventTypesTabs } from "./event-types-tabs";
 const Header = () => {
   return (
     <div className="w-full  flex flex-col items-center space-y-12 py-16 bg-gradient-to-r from-[#020024] to-[#311754] ">
-      <div className="">
-        <div className="w-full flex justify-center text-white">
-          <h1 className="w-[600px]">
-            Each and every day, the Ingress community is mobilizing. From
-            Anomaly Events to Cross Faction Meetups to monthly First Saturday
-            events, Ingress is happening all around you. Learn about the
-            different event types hosted below.
-          </h1>
-        </div>
+      <div className="w-full flex justify-center text-white">
+        <h1 className="w-[900px]">
+          Each and every day, the Ingress community is mobilizing. From Anomaly
+          Events to Cross Faction Meetups to monthly First Saturday events,
+          Ingress is happening all around you. Learn about the different event
+          types hosted below.
+        </h1>
       </div>
+
       <EventTypesTabs />
     </div>
   );
