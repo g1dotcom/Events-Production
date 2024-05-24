996b3e5e41c251dd8663efb18976d6c3075878f1
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-24 03:13:02 +0300

    refactor: Update countries page layout and components

--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -3,12 +3,18 @@ import CitiesSelectModal from "./_components/cities-select-modals";
 import Events from "@/components/events";
 import Map from "@/components/map";
 import { EventsTable } from "@/components/events-table";
-import { BackgroundBeams } from "@/components/bg-beams";
+import { FlipWordsTitle } from "@/components/flip-words-title";
 
 const Countries = () => {
   return (
-    <div className="bg-black w-full h-screen flex flex-col space-y-8">
-      <Map />
+    <div className=" w-full h-screen">
+      <div className="flex w-full p-12">
+        <Map />
+        {/* <div className="w-1/3 flex justify-center items-center h-full bg-red-500">
+          <FlipWordsTitle />
+        </div> */}
+      </div>
+
       <EventsTable />
 
       {/* <BackgroundBeams /> */}
