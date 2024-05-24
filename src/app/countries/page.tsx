c30a09137f4a028bfa0908cb5e4cd80910e2c721
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-24 14:51:01 +0300

    feat: Update countries page layout and components

--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -4,17 +4,17 @@ import Events from "@/components/events";
 import Map from "@/components/map";
 import { EventsTable } from "@/components/events-table";
 import { FlipWordsTitle } from "@/components/flip-words-title";
-import Navbar from "@/components/navbar";
+import Header from "@/components/header";
 
 const Countries = () => {
   return (
-    <div className=" w-full h-screen">
-      <div className="flex w-full p-12">
-        <Map />
-        {/* <div className="w-1/3 flex justify-center items-center h-full bg-red-500">
+    <div className=" w-full h-screen mt-8">
+      <Header />
+      <Map />
+      {/* <div className="w-1/3 flex justify-center items-center h-full bg-red-500">
           <FlipWordsTitle />
         </div> */}
-      </div>
+
       <EventsTable />
       {/* <BackgroundBeams /> */}
     </div>
