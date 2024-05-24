983658655b43349ca21eaca183f47c1c879bb937
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-24 15:10:04 +0300

    feat: Update header with event types description and add EventTypesTabs component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { EventTypesTabs } from "./event-types-tabs";
 
 const Header = () => {
   return (
-    <div className="w-full h-24 flex flex-col bg-gradient-to-r from-[#020024] to-[#311754] "></div>
+    <div className="w-full  flex flex-col items-center space-y-12 py-16 bg-gradient-to-r from-[#020024] to-[#311754] ">
+      <div className="">
+        <div className="w-full flex justify-center text-white">
+          <h1 className="w-[600px]">
+            Each and every day, the Ingress community is mobilizing. From
+            Anomaly Events to Cross Faction Meetups to monthly First Saturday
+            events, Ingress is happening all around you. Learn about the
+            different event types hosted below.
+          </h1>
+        </div>
+      </div>
+      <EventTypesTabs />
+    </div>
   );
 };
 
