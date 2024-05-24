ea9f58f923ddd11d1c8a7a9d96d410338c038217
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-24 14:51:09 +0300

    feat: Add Header component for website header

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-full h-24 flex flex-col bg-gradient-to-r from-[#020024] to-[#311754] "></div>
+  );
+};
+
+export default Header;
