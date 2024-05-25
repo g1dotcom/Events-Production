60e07e51189b70c2e7b226375cdab5ab0bfe38cd
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-26 01:01:13 +0300

    feat: Add font module with Aldrich and Exo fonts

---
+++ src/app/font.ts
@@ -0,0 +1,12 @@
+import { Aldrich, Exo } from "next/font/google";
+
+export const aldrich = Aldrich({
+    weight: "400",
+    subsets: ["latin"],
+  });
+
+export const exo = Exo({
+    weight: "400",
+    subsets: ["latin"],
+    
+  });
