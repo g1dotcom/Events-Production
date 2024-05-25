fb126391ae485ada91beeb7cdbc888e1833857b4
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-26 01:00:55 +0300

    refactor: Update header component with responsive design and improved text alignment

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { EventTypesTabs } from "./event-types-tabs";
+import { exo } from "@/app/font";
 
 const Header = () => {
   return (
-    <div className="w-full  flex flex-col items-center space-y-12 py-16 bg-gradient-to-r from-[#311754] to-[#020024] ">
+    <div
+      className={` ${exo.className} w-full  flex flex-col items-center space-y-12 py-16 bg-gradient-to-r from-[#311754] to-[#020024]`}
+    >
       <div className="w-full flex justify-center text-white">
-        <h1 className="w-[770px] flex justify-center text-center">
+        <h1 className="w-[730px] text-center text-[1.7rem]">
           Each and every day, the Ingress community is mobilizing. From Anomaly
           Events to Cross Faction Meetups to monthly First Saturday events,
           Ingress is happening all around you. Learn about the different event
