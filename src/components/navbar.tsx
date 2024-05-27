0b0fa819ad651b62a3d431e6fe493ba19387b522
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-27 19:12:08 +0300

    refactor: Update Navbar component to remove unused props

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,9 +2,7 @@ import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import React from "react";
 import { Button } from "./ui/button";
 
-type Props = {};
-
-const Navbar = (props: Props) => {
+const Navbar = () => {
   return (
     <div className="h-24 bg-transparent absolute flex justify-end w-full p-4">
       <Button variant={"secondary"} className="w-24 text-lg">
