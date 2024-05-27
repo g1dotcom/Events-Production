da4c830002e77626e21692c813b2813c35f5be5c
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-27 19:12:17 +0300

    refactor: Update Home page layout with Clerk authentication components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
 import Map from "@/components/map";
 import { EventsTable } from "@/components/events-table";
 import Header from "@/components/header";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
-const Home = () => {
+export default function Home() {
   return (
-    <div className=" w-full h-screen   ">
+    <div className=" w-full h-screen">
+      <div className="w-full bg-red-500 h-24">
+        {" "}
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
       <Header />
       <Map />
       <EventsTable />
     </div>
   );
-};
-
-export default Home;
+}
