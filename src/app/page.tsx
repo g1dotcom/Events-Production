dca3649ff04eb2dff95d99efd2740f32f091c1ec
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-29 14:16:01 +0300

    refactor: Lazy load Map component for improved page load performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import Map from "@/components/map";
+import dynamic from "next/dynamic";
+const Map = dynamic(() => import("@/components/map"), { ssr: false });
 import { EventsTable } from "@/components/events-table";
 import Header from "@/components/header";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
@@ -7,15 +8,12 @@ export default function Home() {
   return (
     <div className=" w-full h-screen">
       <div className="w-full bg-red-500 h-24">
-        {" "}
-        <header>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
       <Header />
       <Map />
