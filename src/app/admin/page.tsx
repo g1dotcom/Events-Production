4cfafe5621fd13fda547cc43a9066f4e3415360b
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-31 16:36:20 +0300

    refactor: Update UserAdmin page to include admin check

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -11,7 +11,7 @@ export default async function UserAdmin() {
   if (userId) {
     // Get the User object from Clerk when you need access to the user's information
     const user = await clerkClient.users.getUser(userId);
-    console.log(user.id);
+    // console.log(user.id);
 
     //fetch user from User table prisma db
     const userFromDB = await prisma.user.findUnique({
@@ -19,31 +19,17 @@ export default async function UserAdmin() {
         clerkUserId: user.id,
       },
     });
-    console.log(userFromDB, "userFromDB");
-    // {
-    //   id: 'clwru7mhz0000ce6rzdkm60r9',
-    //   createdAt: 2024-05-29T13:03:31.751Z,
-    //   updatedAt: 2024-05-29T13:03:31.751Z,
-    //   clerkUserId: 'user_2h8u1RXI2bKbKpIj4BQyXeSUHE6',
-    //   admin: false
-    // }
-    //fetch event types from eventTypes table prisma db
+    // console.log(userFromDB, "userFromDB");
+
     const eventTypes = await prisma.eventType.findMany();
-    console.log(eventTypes, "eventTypes");
+    // console.log(eventTypes, "eventTypes");
     //fetch social platforms from socialPlatforms table prisma db
     const socialPlatforms = await prisma.socialPlatform.findMany();
-    console.log(socialPlatforms, "socialPlatforms");
+    // console.log(socialPlatforms, "socialPlatforms");
 
     //check if userFromDB is not null and if userFromDB.admin is true render admin page else render not an admin page
     if (userFromDB && userFromDB.admin) {
-      return (
-        <div>
-          <h1>Admin</h1>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </div>
-      );
+      return permanentRedirect("/create-event");
     } else {
       permanentRedirect("/");
     }
