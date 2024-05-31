250fd40ff17c64e823c7e0600af91548a53bfc0b
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-31 16:36:35 +0300

    refactor: Update Events component to use Prisma database

--- src/components/events.tsx
+++ src/components/events.tsx
@@ -1,39 +1,25 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import supabase from "@/lib/subaseClient";
+import prisma from "@/lib/database";
+import Map from "./map";
 
-export default function Events() {
-  const [events, setEvents] = useState<any[]>([]);
+export default async function EventsTypes() {
+  //fetch event types prisma
+  // const fetchEventTypes = async () => {
+  //   const eventTypes = await prisma.eventType.findMany();
+  //   console.log(eventTypes, "eventTypeseventTypeseventTypeseventTypes");
+  // };
+  // fetchEventTypes();
 
-  useEffect(() => {
-    const fetchEvents = async () => {
-      const { data, error } = await supabase.from("events").select("*");
-      if (data) {
-        setEvents(data);
-        console.log(events);
-      } else {
-        console.error(error);
-      }
-    };
+  //fetch events platforms from events table prisma db
+  const myEvent = await prisma.event.findMany();
+  console.log(myEvent, "myEvent");
 
-    fetchEvents();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const myeventTypes = await prisma.eventType.findMany();
+  console.log(myeventTypes, "myeventTypes");
 
   return (
     <div>
-      <h1>Events</h1>
-      <ul>
-        {events.map((event) => (
-          <li key={event.id}>
-            {event.name} {event.city_id} {event.base_portal_link}{" "}
-            {event.social_platform_id} {event.event_type_id} {event.language}{" "}
-            {event.local_time}
-            {event.time_zone} {event.utc_time} {event.longitute}{" "}
-            {event.latitude}
-          </li>
-        ))}
-      </ul>
+      {" "}
+      <Map myEvent={myEvent} myeventTypes={myeventTypes} />
     </div>
   );
 }
