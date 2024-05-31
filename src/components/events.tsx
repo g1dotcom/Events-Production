fdca1f1733886d6efa6ff931de72a3ee3be75cca
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-31 17:11:22 +0300

    refactor: Update EventsTypes component to use props for data
    This commit refactors the EventsTypes component to use props for events, event types, and social platforms data instead of fetching them from the Prisma database. The events, event types, and social platforms are now passed as props to the EventsTypes component, which improves reusability and flexibility. This change simplifies the component and reduces its dependencies on external data sources.

--- src/components/events.tsx
+++ src/components/events.tsx
@@ -1,25 +1,27 @@
 import prisma from "@/lib/database";
 import Map from "./map";
+import { EventsTable } from "./events-table";
 
 export default async function EventsTypes() {
-  //fetch event types prisma
-  // const fetchEventTypes = async () => {
-  //   const eventTypes = await prisma.eventType.findMany();
-  //   console.log(eventTypes, "eventTypeseventTypeseventTypeseventTypes");
-  // };
-  // fetchEventTypes();
-
-  //fetch events platforms from events table prisma db
+  //fetch data prisma db
   const myEvent = await prisma.event.findMany();
   console.log(myEvent, "myEvent");
 
   const myeventTypes = await prisma.eventType.findMany();
   console.log(myeventTypes, "myeventTypes");
 
+  const mysocialPlatform = await prisma.socialPlatform.findMany();
+  console.log(myeventTypes, "myeventTypes");
+
   return (
     <div>
       {" "}
       <Map myEvent={myEvent} myeventTypes={myeventTypes} />
+      <EventsTable
+        myEvent={myEvent}
+        myeventTypes={myeventTypes}
+        mysocialPlatform={mysocialPlatform}
+      />
     </div>
   );
 }
