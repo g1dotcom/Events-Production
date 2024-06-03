8d4bf77539889562d653776927970413640d877e
Gökhan Süle <thegkhnsl@gmail.com>
2024-06-03 13:59:25 +0300

    refactor: Update database.ts to import PrismaClient and Prisma from '@prisma/client'

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,5 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+// my databe.ts for prisma
+import { PrismaClient, Prisma } from '@prisma/client'
 
 const prisma = new PrismaClient();
 
+
 export default prisma;
+
+
