10582ba42465f837a3303560878b83b51aef351e
Gökhan Süle <thegkhnsl@gmail.com>
2024-06-03 13:59:05 +0300

    refactor: Add CreateEvent component to UserAdmin page
    This commit refactors the UserAdmin page to include the CreateEvent component. The CreateEvent component allows admins to create new events. By adding this component, the UserAdmin page becomes more functional and provides a seamless experience for admins to manage events.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,39 +2,35 @@ import prisma from "@/lib/database";
 import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { permanentRedirect } from "next/navigation";
+import CreateEvent from "@/components/create-events";
 
 export default async function UserAdmin() {
-  // Get the userId from auth() -- if null, the user is not signed in
   const { userId } = auth();
   auth().protect();
 
   if (userId) {
-    // Get the User object from Clerk when you need access to the user's information
     const user = await clerkClient.users.getUser(userId);
-    // console.log(user.id);
-
-    //fetch user from User table prisma db
     const userFromDB = await prisma.user.findUnique({
       where: {
         clerkUserId: user.id,
       },
     });
-    // console.log(userFromDB, "userFromDB");
 
-    const eventTypes = await prisma.eventType.findMany();
-    // console.log(eventTypes, "eventTypes");
-    //fetch social platforms from socialPlatforms table prisma db
-    const socialPlatforms = await prisma.socialPlatform.findMany();
-    // console.log(socialPlatforms, "socialPlatforms");
-
-    //check if userFromDB is not null and if userFromDB.admin is true render admin page else render not an admin page
     if (userFromDB && userFromDB.admin) {
-      return permanentRedirect("/create-event");
+      return (
+        <div>
+          <h1>Admin Page</h1>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton showName />
+          </SignedIn>
+          <CreateEvent />
+        </div>
+      );
     } else {
       permanentRedirect("/");
     }
   }
-
-  // // Get the Backend API User object when you need access to the user's information
-  // const user = await currentUser();
 }
